a019b35ae41cb070b70f3143ff4e37f455cd4592
James Keech <james.a.keech@gmail.com>
2023-01-09 21:44:58 +1100

    Added mouse hover functionality for menu items

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,30 @@
 		$('.hamburger-menu').toggleClass('animate');
 	})
 })();
+
+const $cursor = document.querySelector('#circle');
+const $hoverables = document.querySelectorAll('.hoverable');
+
+
+// Listeners
+document.body.addEventListener('mousemove', onMouseMove);
+for (let i = 0; i < $hoverables.length; i++) {
+  $hoverables[i].addEventListener('mouseenter', onMouseHover);
+  $hoverables[i].addEventListener('mouseleave', onMouseHoverOut);
+}
+
+function onMouseMove(e) {
+	TweenMax.to($cursor, .4, {
+    x: e.pageX - 15,
+    y: e.pageY - 15
+  })
+}
+
+// Hover an element
+function onMouseHover() {
+	$cursor.classList.add("hover")
+}
+
+function onMouseHoverOut() {
+	$cursor.classList.remove("hover")
+}
